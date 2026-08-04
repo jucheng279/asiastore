3bc4c16db3cb35ad88c97e544e5dd3db3bcf79d1
jucheng279 <jucheng279@gmail.com>
2026-08-04 15:38:39 +0200

    Updated AppShell.tsx

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -4,9 +4,9 @@ import DesktopSidebar from './DesktopSidebar';
 
 const AppShell: React.FC = () => {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen overflow-x-hidden">
       <DesktopSidebar />
-      <div className="flex-1 lg:ml-64">
+      <div className="flex-1 lg:ml-64 overflow-x-hidden">
         <Outlet />
       </div>
     </div>
